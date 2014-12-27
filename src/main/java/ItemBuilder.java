1a3af5ce4a007bb69018d05e27fbb116dd80fd10
scoobycoder <scoobycoder@gmail.com>
2014-12-27 15:32:55 -0500

    removing setter for Item Builder Name as it is no longer used

--- src/main/java/ItemBuilder.java
+++ src/main/java/ItemBuilder.java
@@ -5,11 +5,7 @@ public  class ItemBuilder {
 	public ItemBuilder() {
 		item = new Item();
 	}
-	public ItemBuilder setName() {
-		item.setName("does not matter now");
-		return this;
-	}
-	
+
 	public ItemBuilder setQuality(int quality) {
 		item.setQuality(quality);
 		return this;
