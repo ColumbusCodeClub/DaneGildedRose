b2eb5ed2b787713252343be55dbbc482736367f4
scoobycoder <scoobycoder@gmail.com>
2015-01-18 19:01:34 -0500

    full test coverage by coverage tool

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -29,12 +29,39 @@ public class GildedRoseTest {
 	}
 	
 	@Test
+	public void sulfurasStaysSameQualityEveryDay() {
+		Item item = new Item("Sulfuras, Hand of Ragnaros", 5, 5);
+		
+		timePasses(item);
+		
+		assertEquals(5, item.getQuality());
+	}
+	
+	@Test
 	public void agedBrieSellInDateDecreasesEachDay() {
 		Item item = new Item("Aged Brie", 5, 5);
 		
 		timePasses(item);
 		
 		assertEquals(4, item.getSellIn());
+	}
+	
+	@Test
+	public void agedBrieIncreaseInQualityWhenPassingSellInDate() {
+		Item item = new Item("Aged Brie", 0, 5);
+		
+		timePasses(item);
+		
+		assertEquals(7, item.getQuality());
+	}
+	
+	@Test
+	public void agedBrieAtMaxQualityDoesNotIncreaseInQuality() {
+		Item item = new Item("Aged Brie", 5, 50);
+		
+		timePasses(item);
+		
+		assertEquals(50, item.getQuality());
 	}
 	
 	@Test
@@ -56,12 +83,21 @@ public class GildedRoseTest {
 	}
 	
 	@Test
-	public void BackStagePassDecreasesInQualityToZeroWhenPastSellInDate() {
+	public void backStagePassDecreasesInQualityToZeroWhenPastSellInDate() {
 		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5);
 		
 		timePasses(item);
 		
 		assertEquals(0, item.getQuality());
+	}
+	
+	@Test
+	public void backStagePassIncreaseByOneWhenMoreThanElevenDaysToSell() {
+		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 5);
+		
+		timePasses(item);
+		
+		assertEquals(6, item.getQuality());
 	}
 
 }
